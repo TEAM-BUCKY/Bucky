b719a7545e045afcd1c6842b13b76fdac47110f4
Jefta de Graaf <jeftadegraaff@gmail.com>
2022-03-07 16:07:35 +0100

    Ultrasonic working
    Let's go

--- lib/Ultrasonic/Ultrasonic.cpp
+++ lib/Ultrasonic/Ultrasonic.cpp
@@ -7,23 +7,23 @@
 long time; // time it takes in total for sound wave to travel back and forth
 int distance; // distance of object relative to supersonic in cm
 
-Ultrasonic::Ultrasonic(int in1, int in2) {
-    Ultrasonic::in1 = in1;
-    Ultrasonic::in2 = in2;
+Ultrasonic::Ultrasonic(int trigger, int echo) {
+    Ultrasonic::trigger = trigger;
+    Ultrasonic::echo = echo;
 }
 
-void Ultrasonic::setup() {
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    digitalWrite(in1, LOW); 
+void Ultrasonic::ultraSetup() {
+    pinMode(trigger, OUTPUT);
+    pinMode(echo, INPUT);
+    digitalWrite(trigger, LOW); 
 }
 
 
-int Ultrasonic::get() { // calculates and returns distance variab
-    digitalWrite(in1, HIGH);
+int Ultrasonic::read() { // calculates and returns distance variab
+    digitalWrite(trigger, HIGH);
     delayMicroseconds(10);
-    digitalWrite(in1, LOW);
-    time = pulseIn(in2, HIGH);
-    distance = time * 0.034 / 2
+    digitalWrite(trigger, LOW);
+    time = pulseIn(echo, HIGH);
+    distance = time * 0.034 / 2;
     return distance;
 }
