1fee107ad836a0e8643fdb5554e9c79dc7c35124
Jobbet <77113631+Jobbet789@users.noreply.github.com>
2022-04-11 14:59:53 +0200

    Update TSSP2.h
    made variables that you need to access public

--- lib/TSSP2/TSSP2.h
+++ lib/TSSP2/TSSP2.h
@@ -3,7 +3,7 @@
 
 class TSSP2{
     private:
-        int pins[14], maxPulseWidth, maxSensorNumber, activeSensors;
+        int pins[14];
         int ir_num = 14;
         int deltaPulseWidth = 2;
 
@@ -12,6 +12,7 @@ class TSSP2{
         bool getSensorPin(int pin);
         void setAllSensorPinsInput();
         void getAllSensorPulseWidth(float pulseWidth[14], int timeLimit); // also change this to ir_num
+        int maxPulseWidth, maxSensorNumber, activeSensors;
 };
 
 #endif
