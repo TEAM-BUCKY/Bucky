a16c96544c79510ac68998e1f51feaef84689865
Jobbet789 <jobvdveen@gmail.com>
2022-04-12 18:55:28 +0000

    added two functions;
    calculate vector
    
    calculate readius and theta

--- lib/TSSP2/TSSP2.h
+++ lib/TSSP2/TSSP2.h
@@ -7,12 +7,28 @@ class TSSP2{
         int ir_num = 14;
         int deltaPulseWidth = 2;
 
+        const float unitVectorX[14] = {1.000, 1.000, 0.623, 
+        0.223, 0.223, -0.623, -0.901, -1.000, -0.901, -0.623, 
+        -0.223, 0.223, 0.623, 0.000};
+        const float unitVectorY[14] = {0.000, 0.000, 0.782, 
+        0.975, 0.975, 0.782, 0.434, 0.000, -0.434, -0.782, 
+        -0.975, -0.975, 0.782, 0.000};
+
     public:
         TSSP2(int pins[14]); // change this to ir_num
         bool getSensorPin(int pin);
         void setAllSensorPinsInput();
-        void getAllSensorPulseWidth(float pulseWidth[14], int timeLimit); // also change this to ir_num
+        void getAllSensorPulseWidth(int timeLimit); // also change this to ir_num
+        void calcVector();
+        void calcRTfromXY();
         int maxPulseWidth, maxSensorNumber, activeSensors;
+	    int pulseWidth[14] = {};
+
+
+        float IRInfo_x;
+        float IRInfo_y;
+        float IRInfo_radius;
+        float IRInfo_theta;
 };
 
 #endif
