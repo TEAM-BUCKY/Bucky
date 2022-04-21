186c47f59a3bdf76f128fa46412449d21a16b825
Jobbet789 <jobvdveen@gmail.com>
2022-04-21 13:27:36 +0200

    added out of bounds system

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,70 @@
 #include <Wire.h>
 #include <Serial_C.h>
 #include <LightRing.h>
+#include <Motor.h>
 
-int S[5] = {33, 34, 35, 36, 29};  //A0, A1, A2, A3, A4 Respectively on Arduino
+Motor m1 (1,2);
+Motor m2 (22, 23);
+Motor m3 (24, 25);
 
-int CS = 30; 
+Serial_C serial (false);
+
+
+
+int S[5] = {33, 34, 35, 36, 29}; // A0, A1, A2, A3, A4 Respectively on Arduino
+
+int CS = 30;
 int WR = 31;
 int EN = 32;
 
-int INPIN = 27; //IO PIN ON MEGAMUX 
+int INPIN = 27; // IO PIN ON MEGAMUX
 
-void pinSelect(int pinnum){
+void pinSelect(int pinnum) // select pin for lightring
+{
   digitalWrite(WR, LOW);
-    for (int x = 0; x<5; x++){
-      byte state = bitRead(pinnum, x);
-      digitalWrite(S[x], state);
+  for (int x = 0; x < 5; x++)
+  {
+    byte state = bitRead(pinnum, x);
+    digitalWrite(S[x], state);
+  }
 
-    }
-   
   digitalWrite(WR, HIGH);
+}
+
+
+int invertDegrees(int deg) {
+  if (deg == 180 || deg == 0) deg += 180;
+  while (1) {
+    if (deg > 360) deg -= 360;
+    if (deg < 0) deg += 360;
+
+    if (deg > 0 && deg < 360) break;
+
+    // Serial.println(deg);
+
+  }
+
+  return deg;
+}
+
+void move(int deg, int basespeed) { // move functions for motors
+  float pi = 57.29577951; 
+  float speedM1 = -(basespeed) * sin((deg + 180) / pi);
+  float speedM2 = -(basespeed) * sin((deg + 60) / pi); 
+  float speedM3 = -(basespeed) * sin((deg - 60) / pi); 
+
+  m1.move(speedM1);
+  m2.move(speedM2);
+  m3.move(speedM3);
 
 }
-void setup() {
-  // put your setup code here, to run once:
-  for (int x = 0; x < 5; x++) {
+
+
+void setup()
+{
+  // setup for lightring
+  for (int x = 0; x < 5; x++)
+  {
     pinMode(S[x], OUTPUT);
     digitalWrite(S[x], LOW);
   }
@@ -38,29 +79,52 @@ void setup() {
   pinMode(EN, OUTPUT);
   digitalWrite(EN, LOW);
   pinMode(INPIN, INPUT);
- 
+
+  // serial begins/setups
   Serial.begin(115200);
+  serial.setup();
+
+
+  // motor setups
+  m1.setup();
+  m2.setup();
+  m3.setup();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
+
+  // lightring checks 
   int x;
-  for(int i = 0; i<24;i++){
-    x=i;
-    if (i> 12) {
+  int degrees = -1;
+  for (int i = 0; i < 24; i++)
+  {
+    x = i;
+    if (i > 12)
+    {
       x = i + 4;
     }
-  pinSelect(x);
-  int val = analogRead(INPIN);
-  if (val > 550) {
-    Serial.println("out of bounds");
-    continue;
-    
+    pinSelect(x);
+    int val = analogRead(INPIN);
+    if (val > 550)
+    {
+      // Serial.println("out of bounds");
+      degrees = invertDegrees(i * 15);
+      break;
+    }
+    else
+    {
+      // Serial.println("In bounds");
+      degrees = -1;
+    }
   }
-  else {
-    Serial.println("In bounds");
-  }
-  }
-  delay(100);
-}
+  Serial.println(String(degrees));
+  if (degrees != -1) {
+    move(degrees, 100);
+  } else {
 
+    // everything to do other then out of bounds
+    move(0, 100);
+  }
+}
