6852bfa435acd431c74397beb9fe6fe250c75c42
koen1711 <98043234+koen1711@users.noreply.github.com>
2023-01-15 20:15:24 +0100

    CANWrapper added, done

--- include/CANWrapper.cpp
+++ include/CANWrapper.cpp
@@ -6,6 +6,10 @@ void CANWrapper::begin(int baudrate) {
   CAN.begin(baudrate);
 }
 
+void CANWrapper::parsePacket() {
+  CAN.parsePacket();
+}
+
 void CANWrapper::sendData(int id, int* data, int size) {
     CAN.beginPacket(id);
     // check if data is a list if so loop through it and send each element
@@ -19,12 +23,23 @@ int CANWrapper::available() {
     return CAN.available();
 }
 
-char CANWrapper::readData() {
-    char data;
+char* CANWrapper::readData() {
+    char* data = new char[255];
+    int i = 0;
     while (CAN.available()) {
-        data += CAN.read();
+        char byte = (char)CAN.read();
+        data[i++] = byte;
     }
+    data[i]='\0';
     return data;
+}
+
+void CANWrapper::sendDataString(int id, char* data, int size) {
+    CAN.beginPacket(id);
+    for (int i = 0; i < size; i++) {
+        CAN.write(data[i]);
+    }
+    CAN.endPacket();
 }
 
 void CANWrapper::end() {
--- include/CANWrapper.h
+++ include/CANWrapper.h
@@ -5,8 +5,10 @@ class CANWrapper {
     public:
         void begin(int baudrate);
         void sendData(int id, int* data, int size);
+        void sendDataString(int id, char* data, int size);
         int available();
-        char readData();
+        char* readData();
+        void parsePacket();
         void end();
 };
 
