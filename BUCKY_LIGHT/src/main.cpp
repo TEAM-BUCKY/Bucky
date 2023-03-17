cd9b42487fd552acaa08496885b03fae262acb64
koen1711 <98043234+koen1711@users.noreply.github.com>
2023-03-17 12:25:39 +0100

    Rot jefta

--- BUCKY_LIGHT/src/main.cpp
+++ BUCKY_LIGHT/src/main.cpp
@@ -1,20 +1,9 @@
 #include <Arduino.h>
-#include <TSSP2.h>
-
-int pins[16] = {14, 27, 26, 25, 33, 32, 35, 34, 23, 22, 21, 19, 18, 17, 16, 15};
-TSSP2 tssp2(pins);
+#include <LightGate.h>
+#include <LightRing.h>
 
 void setup() {
-  tssp2.setAllSensorPinsInput();
-   Serial.begin(115200);
-   pinMode(13, OUTPUT);
-   digitalWrite(13, HIGH);
 }
 
 void loop() {
-  tssp2.getAllSensorPulseWidth(833);
-  tssp2.calcVector();
-  tssp2.calcRTfromXY();
-  Serial.println(tssp2.IRInfo_theta);   
-  delay(100);  
 }
